e7acb15e8fa29389e83b2ab0ff9094df08bd3033
Daniel Juan <dannijl88web@gmail.com>
2026-04-29 21:08:01 +0200

    feat: navbar with routing and BEM styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import HomePage from './pages/HomePage'
 import MarsRoverPage from './pages/MarsRoverPage'
 import NeoPage from './pages/NeoPage'
 import EpicPage from './pages/EpicPage'
+import NavBar from './components/Navbar'
 
 function App() {
 
   return (
     <BrowserRouter>
+    <NavBar />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/mars-rover' element={<MarsRoverPage />} />
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+import '../styles/navbar.css'
+
+export default function NavBar(){
+    return (
+        <header className='navbar-header'>
+            <div className='navbar-header__title-container'>
+                <h1 className='navbar-header__title'>SpaceExplorer</h1>
+            </div>
+            <nav className='navbar-header__nav'>
+                <ul className='navbar-header__link-container'>
+                    <li><Link to="/" className="navbar-header__link">Inicio</Link></li>
+                    <li><Link to="/mars-rover" className="navbar-header__link">Rover Marte</Link></li>
+                    <li><Link to="/epic" className="navbar-header__link">Epic</Link></li>
+                    <li><Link to="/neo" className="navbar-header__link">Neo</Link></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
