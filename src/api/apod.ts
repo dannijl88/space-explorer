0f035cbabeb0d4bbe42961ddaa0b90c468e4ee4b
Daniel Juan <dannijl88web@gmail.com>
2026-04-30 15:14:26 +0200

    feat: HomePage with APOD section

--- src/api/apod.ts
+++ src/api/apod.ts
@@ -2,7 +2,7 @@ import type { ApodResponse } from "../types/apod";
 import { NASA_API_KEY,NASA_BASE_URL } from "./config";
 
 export const fetchApod = async (): Promise<ApodResponse> => {
-    const response = await fetch(NASA_BASE_URL + "/planetary/apod?api_key=" + NASA_API_KEY)
+    const response = await fetch(NASA_BASE_URL + "/planetary/apod?api_key=" + NASA_API_KEY + "&date=2024-01-01")
     const data = await response.json()
     return data
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,33 @@
 import { useApod } from "../hooks/useApod"
+import '../styles/homePage.css'
 
 export default function HomePage(){
 
     const {apod, isLoading, error} = useApod()
-
+    console.log(apod)
     return(
-        <div>
+        <div className="home-page">
             <p>{isLoading ? "Cargando..." : ""}</p>
             <p>{error ? error : ""}</p>
-            <h1>{apod?.title}</h1>
+            <section className="home-page__hero">
+                <h2 className="home-page__hero-title">Bienvenidos a SpaceExplorer</h2>
+                <p className="home-page__hero-text">Explora el universo a través de los ojos de la NASA. Imágenes diarias, fotos del rover en Marte y datos de asteroides en tiempo real.</p>
+            </section>
+            <section className="home-page__apod">
+                <h2 className="home-page__apod-title">APOD (Astronomy Picture of the Day)</h2>
+                <p className="home-page__apod-description">Cada día la NASA selecciona una imagen o fotografía del universo como imagen del día. Puede ser una nebulosa, un planeta, una galaxia lejana o incluso una foto tomada desde la Tierra. Cada publicación viene acompañada de una explicación escrita por un astrónomo profesional que da contexto a lo que estás viendo</p>
+                <div className="home-page__apod-container">
+                    <h3 className="home-page__apod-container-title">{apod?.title}</h3>
+                    {apod?.media_type === 'image'
+                    ? <img className="home-page__apod-container-img" src={apod?.url} alt={apod?.title} />
+                    : <iframe className="home-page__apod-container-img" src={apod?.url} title={apod?.title} allowFullScreen></iframe>}
+                    <p className="home-page__apod-container-explanation">{apod?.explanation}</p>
+                    <div>
+                        <span>{apod?.date}</span>
+                        <span>{apod?.copyright}</span>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
