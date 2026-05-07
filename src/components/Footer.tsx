718bbd2e1590cf993abc56600c4673205d717e7f
Daniel Juan <dannijl88web@gmail.com>
2026-05-07 10:13:56 +0200

    fix: added mobile breakpoint

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ import '../styles/footer.css'
 export function Footer() {
     return(
         <footer className="footer">
-            <h3>Datos proporcionados por NASA Open APIs</h3>
-            <p>Daniel Juan Lician</p>
-            <p>Github: <a className='footer__link' href={'https://github.com/dannijl88'} target='_blank'>https://github.com/dannijl88</a></p>
-            <p>Linkedin: <a className='footer__link' href={'https://www.linkedin.com/in/danieljuanlician'} target='_blank'>www.linkedin.com/in/danieljuanlician</a></p>
+            <h3 className='footer__title'>Datos proporcionados por NASA Open APIs</h3>
+            <p className='footer__text'>Daniel Juan Lician</p>
+            <p className='footer__text'>Github: <a className='footer__link' href={'https://github.com/dannijl88'} target='_blank'>https://github.com/dannijl88</a></p>
+            <p className='footer__text'>Linkedin: <a className='footer__link' href={'https://www.linkedin.com/in/danieljuanlician'} target='_blank'>www.linkedin.com/in/danieljuanlician</a></p>
         </footer>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,10 +14,10 @@ export default function NavBar(){
             </div>
             <nav className='navbar-header__nav'>
                 <ul className={`navbar-header__link-container ${isOpen ? "navbar-header__link-container--open" : ""}`}>
-                    <li><Link to="/" className="navbar-header__link">Inicio</Link></li>
-                    <li><Link to="/nasa-gallery" className="navbar-header__link">Galeria Nasa</Link></li>
-                    <li><Link to="/epic" className="navbar-header__link">Epic</Link></li>
-                    <li><Link to="/neo" className="navbar-header__link">Neo</Link></li>
+                    <li><Link to="/" className="navbar-header__link" onClick={() => setIsOpen(false)}>Inicio</Link></li>
+                    <li><Link to="/nasa-gallery" className="navbar-header__link" onClick={() => setIsOpen(false)}>Galeria Nasa</Link></li>
+                    <li><Link to="/epic" className="navbar-header__link" onClick={() => setIsOpen(false)}>Epic</Link></li>
+                    <li><Link to="/neo" className="navbar-header__link" onClick={() => setIsOpen(false)}>Neo</Link></li>
                 </ul>
             </nav>
             <img className='navbar-header__hamburger' width={32} height={32} src={hamburgerIcon} onClick={ () => setIsOpen(!isOpen)} />
